6d42b0d0393727ba1062ad26988f8cc0e21b6676
Abdullah <abdullahfaisal7729@gmail.com>
2023-08-17 05:55:15 +0500

    final-code

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,11 +1,8 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./../App.css";
-import { min } from "lodash";
 
 function convertToSecondsFromTime(days, hours, minutes, seconds) {
-  const totalSeconds = seconds + minutes * 60 + hours * 3600 + days * 86400;
-  return totalSeconds;
+  return seconds + minutes * 60 + hours * 3600 + days * 86400;
 }
 
 function convertToTimeFromSeconds(seconds) {
@@ -21,64 +18,26 @@ function convertToTimeFromSeconds(seconds) {
   return { days, hours, minutes, seconds };
 }
 
-const today = new Date().toISOString().split("T")[0];
-const Home = () => {
-  const [date, setDate] = useState({
-    // days: 0,
-    // hours: 0,
-    // minutes: 0,
-    // seconds: 0,
-  });
+const Timer = () => {
   const [hide, setHide] = useState(false);
-  const [finalHours, setFinalHours] = useState(0);
-  const [finalMinutes, setFinalMinutes] = useState(0);
-  const [finalSeconds, setFinalSeconds] = useState(0);
-  const [finalDays, setFinalDays] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [startTimer, setStartTimer] = useState(false);
-  const [finalDate, setFinalDate] = useState({});
-  let numberOfDays;
 
   useEffect(() => {
     let intervalId;
 
     if (startTimer) {
-      // Call the update function once before starting the interval
-      const initialTime = convertToTimeFromSeconds(seconds);
-      // setDate({
-      //   hours: initialTime.hours,
-      //   days: initialTime.days,
-      //   seconds: initialTime.seconds,
-      //   minutes: initialTime.minutes,
-      // });
-      setFinalDays(initialTime.days);
-      setFinalHours(initialTime.hours);
-      setFinalMinutes(initialTime.minutes);
-      setFinalSeconds(initialTime.seconds);
-
       intervalId = setInterval(() => {
         setSeconds((prevSeconds) => prevSeconds - 1);
-        const finalDate = convertToTimeFromSeconds(seconds);
-        setFinalDate(finalDate);
-        setFinalDays(finalDate.days);
-        setFinalHours(finalDate.hours);
-        setFinalMinutes(finalDate.minutes);
-        setFinalSeconds(finalDate.seconds);
-        // setDate({
-        //   hours: finalDate.hours,
-        //   days: finalDate.days,
-        //   seconds: finalDate.seconds,
-        //   minutes: finalDate.minutes,
-        // });
-        // setDate({});
       }, 1000);
     }
 
-    return () => clearInterval(intervalId); // Clean up the interval on component unmount
-  }, [startTimer, finalDate]);
+    return () => clearInterval(intervalId);
+  }, [startTimer]);
+
+  let numberOfDays;
 
   const handleDateChange = (e) => {
-    setDate(e.target.value);
     setHide(!hide);
     console.log("your selected: ", e.target.value);
 
@@ -115,10 +74,13 @@ const Home = () => {
     );
     console.log("In seconds: ", totalSeconds);
     console.log("In time: ", convertToTimeFromSeconds(totalSeconds));
+
     setSeconds(totalSeconds);
     setStartTimer(true);
   };
 
+  const initialCountdown = convertToTimeFromSeconds(seconds);
+  const today = new Date().toISOString().split("T")[0];
   return (
     <div className="center-container">
       <h1>Timer App</h1>
@@ -130,32 +92,18 @@ const Home = () => {
               type="date"
               id="dateInput"
               name="dateInput"
-              min={today}
               onChange={handleDateChange}
-              // onSelect={handleDateChange}
-              // onLoadedData={handleDateChange}
+              min={today}
             ></input>
           </>
         )}
-        {/* Display the timer  */}
         {startTimer && (
           <div>
-            <h1>{console.log(convertToTimeFromSeconds(seconds))}</h1>
-            {/* <h1>
-              {convertToTimeFromSeconds(seconds).days} days,{" "}
-              {convertToTimeFromSeconds(seconds).hours} hours,{" "}
-              {convertToTimeFromSeconds(seconds).minutes} minutes,{" "}
-              {convertToTimeFromSeconds(seconds).seconds} seconds
-            </h1> */}
             <h1>
-              {finalDays} days, {finalHours} hours, {finalMinutes} minutes,{" "}
-              {finalSeconds} seconds
+              {initialCountdown.days} days, {initialCountdown.hours} hours,{" "}
+              {initialCountdown.minutes} minutes, {initialCountdown.seconds}{" "}
+              seconds
             </h1>
-            {/* <h1>
-              {" "}
-              {date["days"]} days, {date["hours"]} hours, {date["minutes"]}{" "}
-              minutes, {date["seconds"]} seconds
-            </h1> */}
           </div>
         )}
       </div>
@@ -163,4 +111,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Timer;
